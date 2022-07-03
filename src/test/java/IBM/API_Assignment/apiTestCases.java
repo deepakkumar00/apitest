f89f73cf49faab00fec4c605869b603a5424eb2f
Deepak Kumar113 <deepak.kumar113@ibm.com>
2022-07-03 23:01:55 +0530

    final

--- src/test/java/IBM/API_Assignment/apiTestCases.java
+++ src/test/java/IBM/API_Assignment/apiTestCases.java
@@ -16,28 +16,29 @@ public class apiTestCases {
 		Object[][] userdata = new Object[1][7];
 		userdata[0][0]= "john1234";
 		userdata[0][1]= "John";
-		userdata[0][2] = 
-		userdata[0][3] =
-		userdata[0][4] =
-		userdata[0][5] = 
+		userdata[0][2] = "Doe";
+		userdata[0][3] = "test@example.com";
+		userdata[0][4] = "john1234";
+		userdata[0][5] = 1234567890;
+		userdata[0][6] = 0;
 		
-		return studentsdata;
+		return userdata;
 		
 	}
 	
-	@Test(enabled = false)
-	public void createuser()
+	@Test(enabled = true, dataProvider = "testdata")
+	public void A1createuser(String uname, String fname, String lname, String email, String pass, int pno, int status)
 	{
 		RestAssured.baseURI = "https://petstore.swagger.io/v2";
 		JSONObject obj = new JSONObject();
 		
-		obj.put("username", "john1234");
-		obj.put("firstName", "John");
-		obj.put("lastName", "Doe");
-		obj.put("email", "test@example.com");
-		obj.put("password", "john1234");
-		obj.put("phone", "1234567890");
-		obj.put("userStatus", 0);
+		obj.put("username", uname);
+		obj.put("firstName", fname);
+		obj.put("lastName", lname);
+		obj.put("email", email);
+		obj.put("password", pass);
+		obj.put("phone", pno);
+		obj.put("userStatus", status);
 		
 		given()
 		.header("content-type", "application/json")
@@ -51,7 +52,7 @@ public class apiTestCases {
 	}
 	
 	@Test(enabled = true)
-	public void getuser()
+	public void A2getuser()
 	{
 		RestAssured.baseURI = "https://petstore.swagger.io/v2";
 		
@@ -65,7 +66,7 @@ public class apiTestCases {
 	}
 	
 	@Test(enabled = true)
-	public void loginuser()
+	public void A3loginuser()
 	{
 		RestAssured.baseURI = "https://petstore.swagger.io/v2";
 		
@@ -80,16 +81,42 @@ public class apiTestCases {
 		
 	}
 	
-	@Test(enabled = false)
-	public void modify()
+	@Test(enabled = true)
+	public void A4modify()
 	{
+		RestAssured.baseURI = "https://petstore.swagger.io/v2";
+		
+		JSONObject obj = new JSONObject();
+		
+		obj.put("firstName", "John1");
+		obj.put("lastName", "Doe");
+		obj.put("email", "test@example.com");
+		obj.put("password", "john1234");
+		obj.put("phone", "1234567890");
+		obj.put("userStatus", 0);
+		
+		given()
+		.header("content-type", "application/json")
+		.body(obj.toJSONString())
+		.when()
+		.put("/user/john1234")
+		.then()
+		.statusCode(200)
+		.log().all();
 		
 	}
 	
-	@Test(enabled = false)
-	public void delete()
-	{
+	@Test(enabled = true)
+	public void A5delete()
+	{ 
+		RestAssured.baseURI = "https://petstore.swagger.io/v2";
 		
+		given()
+		.when()
+		.delete("/user/john1234")
+		.then()
+		.statusCode(200)
+		.log().all();
 	}
 	
 
